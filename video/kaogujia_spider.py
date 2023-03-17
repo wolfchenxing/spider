e7a2d9fe34444c256b8ccb6e2ceb78f453eb6b83
chenyuting <chenyuting@51xf.cn>
2023-03-17 17:47:06 +0800

    打印js渲染后的页面

--- video/kaogujia_spider.py
+++ video/kaogujia_spider.py
@@ -1,7 +1,8 @@
 import time
 
-import CookieUtil
+import cookie_util
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 # 浏览器初始化
@@ -14,7 +15,7 @@ def browser_initial(url):
 if __name__ == '__main__':
     kao_gu_jia_url = 'https://www.kaogujia.com/homeOverview'
     driver = browser_initial(kao_gu_jia_url)
-    CookieUtil.load_cookies(driver)
+    cookie_util.load_cookies(driver)
     # 登录之后，重新访问主页
     driver.get(kao_gu_jia_url)
 
@@ -25,6 +26,10 @@ if __name__ == '__main__':
     # 输入框
     search_input = driver.find_element("xpath", "/html/body/div/div[4]/div[2]/div/div/div[1]/input")
     search_input.send_keys("狗粮")  # 重新这是搜索关键字
+
+    # 打印渲染后的页面
+    html_element = driver.find_element(By.XPATH, '/html')
+    print(html_element.get_attribute('outerHTML'))
 
     time.sleep(1000000)
 
